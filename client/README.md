df8183a325ee9ed4afc4b48f1b696f8f16e9459c
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-18 15:15:16 +0200

    update init resolve

--- client/README.md
+++ client/README.md
@@ -1,26 +1,8 @@
-# graphql_protocol.3.0
+# React + Vite
 
-## About
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-### Getting started
+Currently, two official plugins are available:
 
-`Install graphql client`
-
-```sh
-npm install -g @graphprotocol/graph-cli
-```
-
-`Verify installation`
-
-```sh
-graph 
-```
-
-```sh
-# start build
-graphclient build
-```
-
----------
-
-0xJonaseb11
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+// ...
+// we import types and typed-graphql document from the generated code (`..graphclient/`)
+import { ExampleQueryDocument, ExampleQueryQuery, execute } from '../.graphclient'
+ 
+function App() {
+ const [result, setResult] = React.useState<ExampleQueryQuery>()
+ 
+ useEffect(() => {
+   execute(ExampleQueryDocument, {}).then((result) => {
+     setData(result?.data)
+   })
+ }, [setResult])
+ return (
+   <div className="App">
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <p>Graph Client Example</p>
+       <fieldset>
+         {data && (
+           <form>
+             <label>Data</label>
+             <br />
+             <textarea value={JSON.stringify(data, null, 2)} readOnly rows={25} />
+           </form>
+         )}
+       </fieldset>
+     </header>
+   </div>
+ )
+}
+ 
+export default App
