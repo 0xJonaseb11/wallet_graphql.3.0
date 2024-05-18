8b4a7865b67eb340b881c4783dfa1030b20fc2fa
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-18 15:24:42 +0200

    update init resolve import errors'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react'
-// ...
-// we import types and typed-graphql document from the generated code (`..graphclient/`)
-import { ExampleQueryDocument, ExampleQueryQuery, execute } from '../.graphclient'
- 
+import React, { useEffect, useState } from 'react';
+import logo from "./assets/ball_golden.jpeg";
+import { ExampleQueryDocument, ExampleQueryQuery, execute } from "../src/eg.graphql";
+
 function App() {
- const [result, setResult] = React.useState<ExampleQueryQuery>()
- 
- useEffect(() => {
-   execute(ExampleQueryDocument, {}).then((result) => {
-     setData(result?.data)
-   })
- }, [setResult])
- return (
-   <div className="App">
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <p>Graph Client Example</p>
-       <fieldset>
-         {data && (
-           <form>
-             <label>Data</label>
-             <br />
-             <textarea value={JSON.stringify(data, null, 2)} readOnly rows={25} />
-           </form>
-         )}
-       </fieldset>
-     </header>
-   </div>
- )
+  const [result, setResult] = useState<ExampleQueryQuery | null>(null);
+
+  useEffect(() => {
+    execute(ExampleQueryDocument, {}).then((result) => {
+      setResult(result?.data ?? null);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Graph Client Example</p>
+        <fieldset>
+          {result && (
+            <form>
+              <label>Data</label>
+              <br />
+              <textarea value={JSON.stringify(result, null, 2)} readOnly rows={25} />
+            </form>
+          )}
+        </fieldset>
+      </header>
+    </div>
+  );
 }
- 
-export default App
+
+export default App;
